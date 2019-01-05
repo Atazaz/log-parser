af7317c4bee9504c9944c152a4e1588726790c94
Sophia Shaikh <sophias930@gmail.com>
2019-01-04 23:54:04 -0500

    txt and xml files parsed!

--- config summary/config.js
+++ config summary/config.js
@@ -1,11 +1,18 @@
 /*
 
-TODO:  change this file to match the new input 
+at top of the file in comments:  
+  Product: VALUE
+  Model Number: VALUE
+  Version: VALUE
+  Serial Number: VALUE
+  Read in first 8 features; one row for each 
 
+  out of interface_name>Management</interface_name> pull the ip right below it
+    maybe get it out of the correct parent object
 */
 
 var allText;
-var rowNum = -1;
+var xml;
 
 $(document).ready(() => {
 
@@ -14,12 +21,27 @@ $(document).ready(() => {
         allText = $('#text').val();
         // empty table 
         $('#table').empty();
-        $('#table').append('<tr class="titles"><th>Date</th><th>Time (ms)</th><th>Client IP</th>'+
-        '<th>HTTP Response</th><th>Bytes</th><th>Method</th><th>URL</th><th>Direct/Via</th>'+
-        '<th>Decryption Policy</th><th>Identity</th><th>Access Policy</th><th>Web Category</th>'+
-        '<th>Score</th></tr>');
-        rowNum = -1;
-        parse(allText);
+        $('#table').append('<tr class="titles"> <th>Management IP</th> <th>DNS IP</th><th>Product</th> <th>Model Number</th>'+ 
+        '<th>Version</th> <th>Serial Number</th> <th>Features</th>  </tr>');
+
+         var file = document.getElementById('file').files[0];
+         if (file) {
+             // create reader
+             var reader = new FileReader();
+             reader.readAsText(file);
+             reader.onload = function(e) {
+                 // browser completed reading file
+                //allText = document.createTextNode(e.target.result);
+                allText = e.target.result;
+
+               // $('body').append(allText); 
+
+                xml = $.parseXML(e.target.result);
+
+                parse(allText, xml);
+             };
+
+         }
     })
 
     $('#clear').click(function() {
@@ -27,78 +49,113 @@ $(document).ready(() => {
     })
 })
 
-function makeLogs(allText) {
-    var logs = allText.split("> -");
-}
 
-//  update table w new data on each loop; recursive
-function parse(input) {
+/// update table one time
+function parse(txt, xml) {
+    // txt is the raw text file, not newlines
+    // xml is the jquery object from .parseXML; can find tags etc. 
 
     // create new row to use 
-    rowNum++;
-    $('#table').append('<tr id='+rowNum+'></tr>');
+    $('#table').append('<tr id='+1+'></tr>');
+    var thisRow = $('#'+1);
 
-    // base case: end of input
-    var pieces = input.split(" ");
-    if (pieces[5] == null) {
-        return 0;
+// management ip (using xml) 
+    var interface_name = $(xml).find('interface_name');
+    var interface_legit;
+    for (let i = 0; i < interface_name.length; i++) {
+        if ($(interface_name[i]).text() == "Management") {
+            interface_legit = interface_name[i];
+            break;
+        }
+    }
+    // go to parent and find ip  
+    var ip = $(interface_legit).parent().find("ip");
+    var ipData = $(ip).text();
+    thisRow.append('<td>'+ipData+'</td>');
+
+// dns ip
+    var dns_ip = $(xml).find("dns_ip");
+    //console.log(dns_ip);
+    thisRow.append('<td>'+$(dns_ip[0]).text()+', '+$(dns_ip[1]).text()+'</td>');
+
+// SET UP PARSING FOR TXT 
+    var textArray = txt.split("<!--");
+    var secondArray = textArray[1].split("-->")
+    var comment = secondArray[0];
+
+    var pieces = comment.split(":");
+
+// product 
+    // 'model number' is the ending piece to chop
+    var firstChunk = pieces[1];
+    var firstChunkSplit = firstChunk.split(" ");
+    var product = "";
+
+    let i = 1;
+    while (firstChunkSplit[i] != "") {
+        product += firstChunkSplit[i] + " ";
+        i++;
     }
 
-    // date
-    var utcSeconds = pieces[0];
-    var d = new Date(0); 
-    d.setUTCSeconds(utcSeconds);   
+    thisRow.append('<td>'+product+'</td>');
 
-    var thisRow = $('#'+rowNum);
-    thisRow.append('<td>'+d+'</td>');
-
-    // time
-    thisRow.append('<td>'+pieces[1]+'</td>');
-
-    // client ip
-    thisRow.append('<td>'+pieces[2]+'</td>');
-
-    // http response
-    thisRow.append('<td>'+pieces[3]+'</td>');
-
-    // bytes
-    thisRow.append('<td>'+pieces[4]+'</td>');
-
-    // method
-    thisRow.append('<td>'+pieces[5]+'</td>');
-
-    // url
-    thisRow.append('<td>'+pieces[6]+'</td>');
-
-    // skip 7
-
-    // direct or via
-    thisRow.append('<td>'+pieces[8]+'</td>');
-
-    // skip 9
-
-    // Decryption Policy/Identity/Access Policy:  split by dashes within; 
-        // first chunk can be ignored if there’s a decrypt; 2 then name of decrypt; 
-        // 3 then id; ignore; ignore; ignore; 7 access policy name; ignore (may disappear?)  
-    var decryptChunks = pieces[10].split("-");
-
-    thisRow.append('<td>'+decryptChunks[1]+'</td>');    
-    thisRow.append('<td>'+decryptChunks[2]+'</td>');    
-    thisRow.append('<td>'+decryptChunks[6]+'</td>');    
-
-    var otherChunks = pieces[11].split(",");
-    var web = otherChunks[0].replace("<", "");
-    thisRow.append('<td>'+web+'</td>');    
-    thisRow.append('<td>'+otherChunks[1]+'</td>');    
-
-    // OR split by the last piece (wont work for all data heck) 
-    leftovers = input.split("> -");
-    console.log(input);
-
-    // make sure to add the other stuff at the end! 
-    var fixed = input.substr(leftovers[0].length+3);
-    if (fixed!=null) {
-        parse(fixed);
+// model number
+    // has 'version' as ending piece
+    firstChunk = pieces[2];
+    firstChunkSplit = firstChunk.split(" ");
+    var model = "";
+    i = 1;
+    while (firstChunkSplit[i] != "") {
+        model += firstChunkSplit[i] + " ";
+        i++;
     }
+    thisRow.append('<td>'+model+'</td>');
+
+// version
+    firstChunk = pieces[3];
+    firstChunkSplit = firstChunk.split(" ");
+    var version = "";
+    i = 1;
+    while (firstChunkSplit[i] != "") {
+        version += firstChunkSplit[i] + " ";
+        i++;
+    }
+    thisRow.append('<td>'+version+'</td>');
+
+// serial number
+    firstChunk = pieces[4];
+    firstChunkSplit = firstChunk.split(" ");
+    var serial = "";
+    i = 1;
+    while (firstChunkSplit[i] != "") {
+        serial += firstChunkSplit[i] + " ";
+        i++;
+    }
+    thisRow.append('<td>'+serial+'</td>');
+
+// features (8, one per line)
+    var diffSplit = comment.split('\n');
+    // 8 - 15 are the lines i want! 
+    var allFeatures = "";
+    for (let j = 8; j < 16; j++) {
+        allFeatures += diffSplit[j] + '\n';
+    }
+    thisRow.append('<td id="features">'+allFeatures+'</td>');
+
+    // ADD YOUR OWN STUFF HERE 
 
 }
+
+// table header maker helper
+function addHeader(title) {
+    $('.titles').append('<th>'+title+'</th>');
+}
+
+// SAMPLE CODES
+
+    // getting first instance of a tag
+/*
+var dns_ip = $(xml).find("dns_ip");
+console.log(dns_ip.length);
+thisRow.append('<td>'+dns_ip[0].text());
+*/
